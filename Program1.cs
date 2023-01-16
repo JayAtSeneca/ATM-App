0c8176622680fe9b6f0d67a8d9dbfd43af37dfd0
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-01-16 11:10:37 -0500

    created getters and setters for the CardHolder class

---
+++ Program1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class CardHolder
+{
+    String m_cardNum;
+    int m_pin;
+    String m_firstName;
+    String m_lastName;
+    double m_balance;
+
+    public CardHolder(String cardNum, int pin, String firstName, String lastName,  double balance)
+    {
+        this.m_cardNum = cardNum;
+        this.m_firstName = firstName;
+        this.m_lastName = lastName;
+        this.m_pin = pin;
+        this.m_balance = balance;
+    }
+
+    public String getNum()
+    {
+        return this.m_cardNum;
+    }
+
+    public int getPin()
+    {
+        return this.m_pin;
+    }
+
+    public String getFirstName()
+    {
+        return this.m_firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.m_lastName;
+    }
+
+    public double getBalance()
+    {
+        return this.m_balance;
+    }
+
+    public void setNum(String cardNum)
+    {
+        this.m_cardNum = cardNum;
+    }
+
+    public void setPin(int pin)
+    {
+        this.m_pin = pin;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.m_firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.m_lastName = lastName;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.m_balance = balance;
+    }
+
+    public static void Main(String[] args)
+    {
+
+    }
+}
